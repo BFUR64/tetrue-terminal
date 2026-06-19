60d8a843e7a77bd92bf71d1a6faa51af3c59767e
BFUR64 <iterranceevan@gmail.com>
2026-06-19 21:38:47 +0800

    feat(jlineBackend): Re-add styles

--- terminal-core/src/main/java/io/github/bfur64/terminal/implementations/jline/JLineBackend.java
+++ terminal-core/src/main/java/io/github/bfur64/terminal/implementations/jline/JLineBackend.java
@@ -2,6 +2,7 @@ package io.github.bfur64.terminal.implementations.jline;
 
 import io.github.bfur64.terminal.commands.*;
 import io.github.bfur64.terminal.interfaces.RendererBackend;
+import io.github.bfur64.terminal.output.SGR;
 import io.github.bfur64.terminal.render.Symbol;
 import org.jline.terminal.Size;
 import org.jline.terminal.Terminal;
@@ -13,6 +14,7 @@ import org.jspecify.annotations.NullMarked;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @NullMarked
 public final class JLineBackend implements RendererBackend {
@@ -53,6 +55,8 @@ public final class JLineBackend implements RendererBackend {
                     style.foreground(symbol.fg().r(), symbol.fg().g(), symbol.fg().b());
                 }
 
+                setSGR(style, symbol.SGRs());
+
                 stringBuilder.style(style);
                 stringBuilder.append(symbol.cell());
             }
@@ -62,5 +66,17 @@ public final class JLineBackend implements RendererBackend {
 
         display.update(lines, 0);
         terminal.flush();
+    }
+
+    private void setSGR(AttributedStyle style, Set<SGR> SGRs) {
+        for (SGR sgr : SGRs) {
+            switch (sgr) {
+                case BOLD -> style.bold();
+                case REVERSE -> style.inverse();
+                case UNDERLINE -> style.underline();
+                case ITALIC -> style.italic();
+                case STRIKETHROUGH -> style.crossedOut();
+            }
+        }
     }
 }
